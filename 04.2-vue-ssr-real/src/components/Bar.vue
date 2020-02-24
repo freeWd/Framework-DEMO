5e27909232a72856019cd4a6587a5c0c9c31c5d0
吴迪 <wudi@seczone.cn>
2020-02-24 14:36:23 +0800

    重命名

--- 04.2-vue-ssr-real/src/components/Bar.vue
+++ 04.2-vue-ssr-real/src/components/Bar.vue
@@ -16,11 +16,11 @@ export default {
   },
   // 写的代码都是异步的，全部采用promsie
   // 规定只有页面级的组件才能使用
-//   asyncData({ store }) {
-//     // 异步数据 这个方法在服务端执行，在客户端是不会执行的
-//     const newValue = "new demo name async";
-//     return store.dispatch("set_username", newValue);
-//   },
+  //   asyncData({ store }) {
+  //     // 异步数据 这个方法在服务端执行，在客户端是不会执行的
+  //     const newValue = "new demo name async";
+  //     return store.dispatch("set_username", newValue);
+  //   },
   mounted() {
     const newValue = "new demo name";
     this.$store.dispatch("set_username", newValue);
@@ -34,7 +34,7 @@ export default {
     },
     ...mapActions(["set_username"]),
     changeNameAsync() {
-      console.log('xxx');
+      console.log("xxx");
       this["set_username"]("click async demo name").then(msg => {
         console.log(msg);
       });
